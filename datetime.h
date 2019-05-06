24f430ba023e626adc9a48731c4617e9e978c728
majid farhadi <49880128+9738173@users.noreply.github.com>
2019-05-07 00:33:21 +0430

    finished

--- datetime.h
+++ datetime.h
@@ -7,5 +7,6 @@ public:
 	datetime(int _year, int _month, int _day, int _hour, int _minute, int _second);
 	void edit_datetime(int _year, int _month, int _day, int _hour, int _minute, int _second);
 	int * show_datetime()const;
+	int * diff_to_now()const;
 	~datetime();
 };
