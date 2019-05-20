a138d0733f0b833122609bca995ebf5c24b3a145
Ali Ghadiri <ali.ghadiri78@gmail.com>
2019-05-20 10:08:04 +0430

    organizer cpp finished

--- organizer.cpp
+++ organizer.cpp
@@ -1,1 +1,37 @@
 #include "organizer.h"
+organizer::organizer(mode _md)
+{
+	md = _md;
+}
+void organizer::insert_tour(tournament ob)
+{
+	tournaments.push_back(ob);
+}
+void organizer::delete_tour(tournament ob)
+{
+	tournaments.erase(ob);
+}
+vector <tournament> organizer::show_tours()
+{
+	return tournaments;
+}
+void organizer::sort_teams()
+{
+	sort(teams.begin(), teams.end(), boolsort);
+}
+vector <team> organizer::show_teams() const
+{
+	return teams;
+}
+vector <athlete> organizer::show_athletes() const
+{
+	return athletes;
+}
+mode organizer::show_mode()const
+{
+	return md;
+}
+organizer::~organizer()
+{
+
+}
