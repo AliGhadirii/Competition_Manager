afa4d98faa6e56c025d6798ef23c10c9407d2586
SirAfshin <49054585+SirAfshin@users.noreply.github.com>
2019-05-26 23:35:18 +0430

    Update datetime.h

--- Login and time and date/datetime.h
+++ Login and time and date/datetime.h
@@ -1,19 +1,17 @@
 #pragma once
 #include"Header.h"
-#include <ctime>
-#include<time.h>
 
 class datetime
 {
-	int year,month,day;
-	int hour,min,sec;
+	int td[6];
+	int tddif[6];
 	int tdNow[6];
 	void getNewTimeDate();
 public:
+	datetime();
 	datetime(int y,int m,int d,int h,int mi,int s);
 	void edit_datetime(int _year, int _month, int _day, int _hour, int _minute, int _second);
-	void show_datetime(int *time);
-	void diff_to_now(int *tddif);
+	int * show_datetime();
+	int * diff_to_now();
 	~datetime();
 };
-
